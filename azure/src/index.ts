dec4d016f7682de6caf3300e3d586ceb089bbbdc
Nabarun <64539836+nabarvn@users.noreply.github.com>
2025-10-21 23:25:28 -0400

    Set up main entry point for Azure Functions

---
+++ azure/src/index.ts
@@ -0,0 +1,7 @@
+// src/index.ts - Main entry point for Azure Functions
+// This file imports all function modules to ensure they're registered
+
+import "./functions/create-image";
+import "./functions/generate-suggestion";
+import "./functions/list-images";
+import "./functions/ping-redis";
